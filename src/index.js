aa7965e47df98e00c64ebc8fe1f472444a826e21
ADIKryvonos <108137330+ADIKryvonos@users.noreply.github.com>
2023-05-28 20:58:30 +0300

    add layout

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const BAZE_URL = "https://voodoo-sandbox.myshopify.com/";
+const BAZE_URL = 'https://voodoo-sandbox.myshopify.com/';
 
 export async function getData() {
   const response = await axios.get(`${BAZE_URL}products.json?limit=12`);
@@ -7,5 +7,5 @@ export async function getData() {
 }
 
 getData()
-  .then((resp) => console.log(resp.products))
-  .catch((err) => console.log(err));
+  .then(resp => console.log(resp.products))
+  .catch(err => console.log(err));
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*"],
+  content: ['./**/*'],
   theme: {
-    extend: {},
+    screens: {
+      lg: '768px',
+      md: '480px',
+    },
+    container: {
+      center: true,
+    },
+
+    extend: {
+      fontFamily: {
+        baze: "'Space Grotesk', sans-serif",
+        mono: "'Space Mono', monospace",
+      },
+      colors: {
+        bgColor: '#FCF7E6',
+        mainTextColor: '#000000',
+        secondTextColor: '#FFFFFF',
+        btnColor: '#ED5E21',
+      },
+      padding: {
+        des: '48px',
+        mob: '24px',
+      },
+      margin: {
+        des: '120px',
+        mob: '24px',
+      },
+    },
   },
   plugins: [],
 };
